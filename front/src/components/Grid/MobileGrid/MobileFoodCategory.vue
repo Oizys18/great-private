3f32ee72629f09ffb51e03f050b5dd4ed792f979
Oizys18 <oizys18@gmail.com>
2020-02-14 18:53:57 +0900

    Add mob grid

--- front/src/components/Grid/MobileGrid/MobileFoodCategory.vue
+++ front/src/components/Grid/MobileGrid/MobileFoodCategory.vue
@@ -1,41 +1,47 @@
 <template>
   <div>
-    <div v-if="showGrid===0" class="category-grid">
+    <div v-if="showGrid===-1" class="m-category-grid">
       <div
         v-for="(food, idx) in foodlist1"
         :key="`${food}-${idx}`"
-        class="center-box"
+        class="m-center-box"
       >
-        <button @click="changeview(idx)">
-          <img class="category-image" :src="category1Img[idx]" />
+        <button @click="changeview(food)">
+          <img class="m-category-image" :src="category1Img[idx]" />
         </button>
       </div>
-      <div class="center-box">Location</div>
+      <div class="m-center-box">Location</div>
       <div
         v-for="(food, idx) in foodlist2"
         :key="`${food}-${idx}`"
-        class="center-box"
+        class="m-center-box"
       >
-        <button @click="changeview(idx)">
-          <img class="category-image" :src="category2Img[idx]" />
+        <button @click="changeview(food)">
+          <img class="m-category-image" :src="category2Img[idx]" />
         </button>
       </div>
     </div>
 
     <div v-else>
-      
+      <button @click="go"> hello</button>
+      <MobileGridItem :num="store" :bookmark="bookmark"/>
     </div>
   </div>
 </template>
 
 <script>
+import MobileGridItem from "@/components/Grid/MobileGrid/MobileGridItem.vue"
 import "@/assets/style/css/gridStyle.css";
 export default {
   name: "MobileFoodCategory",
+  components:{
+    MobileGridItem
+  },
+  props: ["num", "bookmark"],
   data() {
     return {
       foodlist1: [0, 1, 2, 3],
-      foodlist2: [0, 1, 2, 3],
+      foodlist2: [4, 5, 6, 7],
       category1Img: [
         "https://i.imgur.com/XIZOsMo.png",
         "https://i.imgur.com/iBTK8eT.png",
@@ -48,17 +54,26 @@ export default {
         "https://i.imgur.com/IF2chC8.png",
         "https://i.imgur.com/6V4QzGn.png"
       ],
-      showGrid:0,
+      showGrid:-1,
+      
     };
   },
   methods: {
     changeview(idx) {
       console.log("hello");
       console.log(idx);
+      this.showGrid = idx
       // this.$store.state.viewcategory = this.key;
       // this.showGrid = 1
-      
     },
+    go(){
+      this.showGrid = -1
+    }
+  },
+  computed:{
+    store(){
+      return this.showGrid
+    }
   }
 };
 </script>
